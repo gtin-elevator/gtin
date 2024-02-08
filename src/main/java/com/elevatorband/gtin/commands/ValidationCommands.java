f04773bdd580d0284de7749350ad9a726cc26937
Jennifer Smale <JenniferSmale7@gmail.com>
2024-02-08 08:08:35 -0500

    validation commands framework

--- src/main/java/com/elevatorband/gtin/commands/ValidationCommands.java
+++ src/main/java/com/elevatorband/gtin/commands/ValidationCommands.java
@@ -1,7 +1,17 @@
 package com.elevatorband.gtin.commands;
 
 import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
 public class ValidationCommands {
+    String upcToValidate = filler;
+    boolean isValid = false;
+    if (upcToValiate)
+    {
+
+    }
+    return isValid;
+
+
 }
